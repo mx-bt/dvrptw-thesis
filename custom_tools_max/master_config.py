966c35e4bdd494dce13999f3265ff032423ea7fa
mx-bt <151383144+mx-bt@users.noreply.github.com>
2025-02-01 15:58:58 +0100

    Create master_config.py

---
+++ custom_tools_max/master_config.py
@@ -0,0 +1,7 @@
+# For simulation runs
+
+LAMBDA_overt_time_for_drivers=1.029630
+LAMBDA_utilisation_of_backup_trucks=0.621997
+LAMBDA_outsourcing_deliveries=1.664067
+LAMBDA_early_service=0.877095
+LAMBDA_late_service=1.71600 
